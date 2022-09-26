b6c57f213409cff195e6761c60a595faa2e3fe11
Paulitchos <74797330+Paulitchos@users.noreply.github.com>
2022-09-26 16:22:22 +0100

    Primeira Parte

--- Aula_P1/Aula_P1/Controllers/HomeController.cs
+++ Aula_P1/Aula_P1/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Aula_P1.Controllers
             return View();
         }
 
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
